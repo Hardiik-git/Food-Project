cb156d387feb3799c774f196d1a27841eb7f71c6
Hardik <hardiik003@gmail.com>
2023-12-24 21:03:01 +0530

    New

--- Script/spice.js
+++ Script/spice.js
@@ -5,10 +5,8 @@ navresponsBtn.addEventListener("click", () => {
   if (navBtnClick === "none") {
     navBtnClick = "block";
     document.querySelector(".nav_items").style.display = "flex";
-    document.querySelector(".nav_items").style.maxWidth = "992px";
   } else {
     navBtnClick = "none";
     document.querySelector(".nav_items").style.display = "none";
-    document.querySelector(".nav_items").style.maxWidth = "992px";
   }
 });
